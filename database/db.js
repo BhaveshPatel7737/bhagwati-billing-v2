a2dff438f2ce63e94cb633a4a4df0a94a00ae460
BhaveshPatel7737 <144474602+BhaveshPatel7737@users.noreply.github.com>
2026-02-24 16:24:50 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://lbthofzpyybmwwfekkkr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxidGhvZnpweXlibXd3ZmVra2tyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2NjY0MzQsImV4cCI6MjA4NzI0MjQzNH0.T8SMt7yeBGsWARPhguH91fHMGbN3QgMYmJ1KQRQX_WE';  // Your anon key
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const db = createClient(supabaseUrl, supabaseKey);
 
-module.exports = supabase;
+module.exports = db;
